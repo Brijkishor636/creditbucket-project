4b06b51990d5f29971ca5442540d18818183e7b5
Shambhu Kumar <148451537+ransa17@users.noreply.github.com>
2026-03-09 18:06:47 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -50,7 +50,7 @@ export default function Home() {
           <ImpactSection />
           <LendingPartnersSection />
       </div>
-      <Problem />
+      
       <Solution />
       <WhyPartner />
 
